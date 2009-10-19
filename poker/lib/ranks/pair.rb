ac6ce79891780f4d3b73be786ee7968cdf918f8c
Dsantoro <soulnafein@gmail.com>
2009-10-19 15:33:33 +0100

    Implemented kickers comparison for Pair

--- poker/lib/ranks/pair.rb
+++ poker/lib/ranks/pair.rb
@@ -1,12 +1,15 @@
 class Pair < Rank
-  def initialize(face)
+  def initialize(face, kickers = [])
     @face = face
+    @kickers = kickers
     super("Pair")
   end
 
   def self.find_in hand
-    pairs = hand.tuples_with_length(2)
-    Pair.new(pairs.first) if not pairs.first.nil?  
+    pair = hand.tuples_with_length(2).first
+    kickers = hand.cards.reject { |card| card.face == pair }
+
+    Pair.new(pair, kickers) if not pair.nil?
   end
 
   def value
@@ -14,9 +17,28 @@ class Pair < Rank
   end
 
   def tie_breaking(other_pair)
-    @face <=> other_pair.face
+    face_comparison = @face <=> other_pair.face
+    if face_comparison == 0
+      kickers_comparison(other_pair)
+    else
+      face_comparison
+    end
   end
 
   protected
-    attr_reader :face
+  attr_reader :face
+  attr_reader :kickers
+
+  private
+  def kickers_comparison(other)
+    faces = @kickers.map { |card| card.face }
+    other_faces = other.kickers.map { |card| card.face }
+    top_card = (faces - other_faces).sort.last
+    other_top_card = (other_faces - faces).sort.last
+    if top_card && other_top_card
+      top_card <=> other_top_card
+    else
+      0
+    end
+  end
 end
--- poker/spec/ranks/pair_spec.rb
+++ poker/spec/ranks/pair_spec.rb
@@ -2,13 +2,21 @@ require 'spec/spec_helper.rb'
 
 describe Pair do
   it "should return the highest pair" do
-    hand = Hand.new("5h Ts Td 5h")
+    hand = Hand.new("5h Ts Td 5s 8s 7d")
 
-    Pair.find_in(hand).should == Pair.new(Face::TEN)
+    Pair.find_in(hand).should == Pair.new(Face::TEN, Hand.new("5h 5s 8s 7d").cards)
   end
 
   it "should be better than a lower pair" do
     Pair.new(Face::TEN).should > Pair.new(Face::EIGHT)
+  end
+
+  it "should compare kickers when comparing with same pair" do
+    winning_pair = Pair.new(Face::TEN, Hand.new("8h 4d 3s").cards)
+    losing_pair = Pair.new(Face::TEN, Hand.new("2d 8h 4s").cards)
+
+    winning_pair.should > losing_pair
+    losing_pair.should < winning_pair
   end
 
   it "should be equal to a pair with the same face" do
