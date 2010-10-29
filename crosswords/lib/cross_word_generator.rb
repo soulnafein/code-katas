ccf36d6fe23b7f80ceee9afec41664156d4460c1
David Santoro <soulnafein@gmail.com>
2010-10-29 22:07:57 +0100

    Crossword so far

--- crosswords/lib/cross_word_generator.rb
+++ crosswords/lib/cross_word_generator.rb
@@ -1,33 +1,41 @@
 class CrossWordGenerator
   def generate_from(input)
-    print_horizontal_border(input) +
-    print_center(input) +
-    print_center(input) +
-    print_horizontal_border(input)
+    result = ""
+    is_first_line = true
+    input.each_line do |line|
+      #line = line.gsub(/^[X]*/) { |s| "A"*s.size}
+      result << print_horizontal_border(line.strip) if is_first_line
+      result << print_center(line.strip)
+      result << print_center(line.strip)
+      result << print_horizontal_border(line.strip)
+      is_first_line = false
+    end
+    result
   end
 
   private
-  def print_block(type)
-    if type == "X"
-      "######"
-    else
-      "#    #"
-    end
+  BUILDING_BLOCKS_CENTER = { "X" => "######", "_" => "#    #", "A" => "      "}
+  BUILDING_BLOCKS_BORDER = { "X" => "######", "_" => "######", "A" => "      "}
+  def print_center_block(type)
+    BUILDING_BLOCKS_CENTER[type]
+  end
+
+  def print_border_block(type)
+    BUILDING_BLOCKS_BORDER[type]
   end
 
   def print_center(input)
     result = ""
     input.each_char do |char|
-      result = result[0..-2] + print_block(char)
+      result = result[0..-2] + print_center_block(char)
     end
     result + "\n"
   end
 
   def print_horizontal_border(input)
-    number_of_characters_to_print = input.size * 6 - (input.size-1)
     result = ""
-    number_of_characters_to_print.times do
-      result << "#"
+    input.each_char do |char|
+      result = result[0..-2] + print_border_block(char)
     end
     result + "\n"
   end
--- crosswords/spec/crosword_gen_spec.rb
+++ crosswords/spec/crosword_gen_spec.rb
@@ -1,31 +1,36 @@
 require 'spec_helper'
 
 describe CrossWordGenerator do
-  it "should substitute X with a massive block of hashes" do
-    crossword_generator = CrossWordGenerator.new
-    expected_result = "######\n" +
-                      "######\n" +
-                      "######\n" +
-                      "######\n"
-
-    crossword_generator.generate_from("X").should == expected_result
-  end
-
-  it "should substitute X with a massive block of hashes" do
-    crossword_generator = CrossWordGenerator.new
-    expected_result = "######\n" +
-                      "#    #\n" +
-                      "#    #\n" +
-                      "######\n"
-
-    crossword_generator.generate_from("_").should == expected_result
-  end
   it "should substitute x and _ with relevant blocks for a line" do
+    crossword_generator = CrossWordGenerator.new
+    result =  "#####################\n" +
+              "#    ######    #    #\n" +
+              "#    ######    #    #\n" +
+              "#####################\n"
+    crossword_generator.generate_from("_X__").should == result
+  end
+
+  it "should process multiline input" do
     crossword_generator = CrossWordGenerator.new
     result =  "################\n" +
               "######    #    #\n" +
               "######    #    #\n" +
+              "################\n" +
+              "#    ######    #\n" +
+              "#    ######    #\n" +
               "################\n"
-    crossword_generator.generate_from("X__").should == result
+    crossword_generator.generate_from("X__\n_X_").should == result
+  end
+
+  it "should remove filled blocks from the outside of the crossword" do
+    crossword_generator = CrossWordGenerator.new
+    result =  "################\n" +
+              "######    ######\n" +
+              "######    ######\n" +
+              "################\n" +
+              "#    #    #    #\n" +
+              "#    #    #    #\n" +
+              "################\n"
+    crossword_generator.generate_from("X_X\n___").should == result
   end
 end
